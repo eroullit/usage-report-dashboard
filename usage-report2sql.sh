6f4312f9bfce7cc73a03f4f36421fb6fbea9be1e
Emmanuel Roullit <eroullit@github.com>
2023-06-23 18:09:46 +0200

    db: prepare price column before gettings started
    Signed-off-by: Emmanuel Roullit <emmanuel.roullit@gmail.com>
    Signed-off-by: Emmanuel Roullit <eroullit@github.com>

--- usage-report2sql.sh
+++ usage-report2sql.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 sqlite-utils insert usage.db usage "$1" --csv --detect-types
+
+# Add a dedicated price column and calculate a per-item price
+sqlite-utils add-column usage.db usage price float --not-null-default 0.0
+sqlite-utils usage.db 'UPDATE usage SET Price = (Quantity * "Price Per Unit ($)" * Multiplier)'
