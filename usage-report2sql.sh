0f946c7d07011caf1ab2da4cad3ca051401ca301
Emmanuel Roullit <eroullit@github.com>
2023-06-30 10:21:42 +0200

    dash: add price quantity heatmap
    Signed-off-by: Emmanuel Roullit <eroullit@github.com>

--- usage-report2sql.sh
+++ usage-report2sql.sh
@@ -3,5 +3,5 @@
 sqlite-utils insert usage.db usage "$1" --csv --detect-types
 
 # Add a dedicated price column and calculate a per-item price
-sqlite-utils add-column usage.db usage price float --not-null-default 0.0
+sqlite-utils add-column usage.db usage price float
 sqlite-utils usage.db 'UPDATE usage SET Price = (Quantity * "Price Per Unit ($)" * Multiplier)'
