f10cead0c12cc88b02c031650f885e202881fea6
Emmanuel Roullit <eroullit@github.com>
2023-07-10 12:15:54 +0000

    run: add script to prepare and boot dashboards
    Signed-off-by: Emmanuel Roullit <eroullit@github.com>

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+test -z "$1" && ! test -f usage.db && echo "Missing Usage Report .csv" && exit 1
+
+# Prepare database a new usage report has been supplied
+test -n "$1" && ./usage-report2sql.sh "$1"
+
+# Start dashboard when database has been prepared
+datasette usage.db --metadata metadata.yml --setting max_returned_rows 200000
