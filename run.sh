f7987bfd5cb89c51e9f6209e33d38c4fca99a50a
Emmanuel Roullit <eroullit@github.com>
2023-08-09 17:11:16 +0200

    dash: reload datasette when metadata are changed
    Signed-off-by: Emmanuel Roullit <eroullit@github.com>

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ test -z "$1" && ! test -f usage.db && echo "Missing Usage Report .csv" && exit 1
 test -n "$1" && ./usage-report2sql.sh "$1"
 
 # Start dashboard when database has been prepared
-datasette usage.db --metadata metadata.yml --setting max_returned_rows 200000
+datasette --reload usage.db --metadata metadata.yml --setting max_returned_rows 200000
